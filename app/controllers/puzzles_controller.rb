24296edae9c07e004d1b1edaf4ce4c5727c32e12
Harry Boyes <harry.boyes@gmail.com>
2025-01-04 09:03:34 +0000

    Improve puzzle show pages

--- app/controllers/puzzles_controller.rb
+++ app/controllers/puzzles_controller.rb
@@ -1,5 +1,16 @@
 class PuzzlesController < ApplicationController
+  before_action :set_puzzle, only: [:show]
+
   def index
     @puzzles = Puzzle.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_puzzle
+    @puzzle = Puzzle.find(params[:id])
   end
 end
