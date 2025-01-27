82d5287a9cf53aafffeaba18b6ff6dede10720bf
Harry Boyes <harry.boyes@gmail.com>
2025-01-27 19:09:27 +0000

    Add validators to contact form

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,6 @@
 class Contact < ApplicationRecord
   belongs_to :user
   validates :user_id, uniqueness: true
+
+  validates_presence_of :name, :phone, :address_1, :address_2, :city, :postal_code
 end
