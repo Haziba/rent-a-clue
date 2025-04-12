5ff1ae1e59d170e8fd94d9a4ff56fa14f398d149
Harry Boyes <harry.boyes@gmail.com>
2025-04-12 21:33:53 +0100

    Set up new email template

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: '"Rent a Clue" <info@mail.rent-a-clue.co.uk>'
+  default from: 'Rent a Clue <info@mail.rent-a-clue.co.uk>'
   layout "mailer"
 end
